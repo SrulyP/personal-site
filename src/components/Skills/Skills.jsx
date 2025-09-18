ada3fe975d0d5c0ccb37da3db8da2be001350be5
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-09-18 14:28:25 -0700

    Add skills component to list areas of expertise

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,30 @@
+import styles from './SkillsStyles.module.css';
+
+function Skills() {
+    const languages = ['Python', 'JavaScript', 'C', 'x86 Assembly', 'SQL'];
+    const tools = ['React', 'Flask', 'HTML', 'CSS', 'Git', 'Github'];
+
+    return (
+        <section id="skills" className={styles.container}>
+            <div>
+                <h2>Languages</h2>
+                <ul className="languageList">
+                    {languages.map((lang, index) => (
+                        <li key={index}>{lang}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <h2>Tools</h2>
+                <ul className="toolsList">
+                    {tools.map((tool, index) => (
+                        <li key={index}>{tool}</li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+export default Skills;
