448f0f32c96ccd5d9da29f520a86d570ad3a64f9
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-09-18 13:59:12 -0700

    Add projects component to display projects

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,59 @@
+import ProjectCard from '../../common/ProjectCard';
+import styles from './ProjectsStyles.modules.css';
+import battlefield from '../../assets/battlefield.png';
+import weather from '../../assets/weather.png';
+import todo from '../../assets/todo.png';
+import library from '../../assets/library.png';
+
+function Projects() {
+    return (
+        <section id="projects" className={styles.projectsContainer}>
+            <ProjectCard
+                src={battlefield}
+                link="https://github.com/SrulyP/battleship"
+                projectTitle="Battlefield"
+                projectDesc="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi ab, expedita deleniti eveniet assumenda eos voluptatem
+                    asperiores harum soluta vel similique suscipit repellendus
+                    error minus facilis veritatis placeat nihil culpa
+                    dignissimos architecto enim delectus odit esse. Hic vel
+                    similique debitis."
+            />
+            <ProjectCard
+                src={weather}
+                link="https://github.com/SrulyP/weather-app"
+                projectTitle="Weather App"
+                projectDesc="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi ab, expedita deleniti eveniet assumenda eos voluptatem
+                    asperiores harum soluta vel similique suscipit repellendus
+                    error minus facilis veritatis placeat nihil culpa
+                    dignissimos architecto enim delectus odit esse. Hic vel
+                    similique debitis."
+            />
+            <ProjectCard
+                src={todo}
+                link="https://github.com/SrulyP/to-do-list"
+                projectTitle="To-Do App"
+                projectDesc="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi ab, expedita deleniti eveniet assumenda eos voluptatem
+                    asperiores harum soluta vel similique suscipit repellendus
+                    error minus facilis veritatis placeat nihil culpa
+                    dignissimos architecto enim delectus odit esse. Hic vel
+                    similique debitis."
+            />
+            <ProjectCard
+                src={library}
+                link="https://github.com/SrulyP/library-project"
+                projectTitle="Library App"
+                projectDesc="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quasi ab, expedita deleniti eveniet assumenda eos voluptatem
+                    asperiores harum soluta vel similique suscipit repellendus
+                    error minus facilis veritatis placeat nihil culpa
+                    dignissimos architecto enim delectus odit esse. Hic vel
+                    similique debitis."
+            />
+        </section>
+    );
+}
+
+export default Projects;
