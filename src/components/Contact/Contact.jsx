dd09ba7c4c4785af8aaabe3004ef2dddf9179fa4
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-09-18 14:55:43 -0700

    Add socials section with icons for each social

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,41 @@
 import styles from './ContactStyles.module.css';
+import resume from '../../assets/resume.pdf';
+import githubIcon from '../../assets/github.svg';
+import linkedinIcon from '../../assets/linkedin.svg';
+import emailIcon from '../../assets/email.svg';
+import resumeIcon from '../../assets/resume.svg';
 
 function Contact() {
     return (
         <section id="contact" className={styles.container}>
             <h1 className="sectionTitle">Contact</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+                quia?
+            </p>
+            <div className="socials">
+                <a
+                    href="https://www.linkedin.com/in/israelpolasak/"
+                    target="_blank"
+                >
+                    <img src={linkedinIcon} alt="linkedin icon" />
+                    <span>linkedin.com/in/israelpolasak</span>
+                </a>
+                <a href="https://github.com/SrulyP" target="_blank">
+                    <img src={githubIcon} alt="github icon" />
+                    <span>github.com/SrulyP</span>
+                </a>
+                <a href="mailto:israelpolasak@gmail.com">
+                    <img src={emailIcon} alt="email icon" />
+                    <span>israelpolasak@gmail.com</span>
+                </a>
+                <a href={resume}>
+                    <img src={resumeIcon} alt="resume icon" />
+                    <button>Resume</button>
+                </a>
+            </div>
+
+            <p>Leave Message</p>
             <form action="">
                 <div className="formRow">
                     <label htmlFor="name" hidden>
