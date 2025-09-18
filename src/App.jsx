87a00832469e202bfc0b5239785b9186b021a1c0
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-09-18 15:01:02 -0700

    Add all components into the app section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
-    return <>App</>;
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Skills />
+            <Projects />
+            <Contact />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
