d630365bbb5772561a939b196be02f6e60cc9fc7
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-09-18 15:08:55 -0700

    Add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <section>
+            <p>Israel</p>
+            <div className="links">
+                <a href="#projects">Projects</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </section>
+    );
+}
+
+export default Header;
