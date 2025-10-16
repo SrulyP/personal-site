170659f0bf73dbb9acf461952f5030edb879351c
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-10-16 15:55:08 +0300

    Add personal site placeholder in project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,12 +3,20 @@ import battlefield from '../assets/battlefield.png';
 import weather from '../assets/weather.png';
 import todo from '../assets/todo.png';
 import library from '../assets/library.png';
+import thisSite from '../assets/thisSite.png';
 import ProjectCard from '../common/ProjectCard';
 
 function Projects() {
     return (
         <section id="projects" className="projectsSection">
             <h2>My Projects</h2>
+            <ProjectCard
+                src={thisSite}
+                link="https://github.com/SrulyP/personal-site"
+                projectTitle="Personal Site"
+                projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
+                technologies={['React', 'Vite', 'JSX', 'CSS']}
+            />
             <ProjectCard
                 src={battlefield}
                 link="https://github.com/SrulyP/battleship"
@@ -28,14 +36,14 @@ function Projects() {
                 link="https://github.com/SrulyP/to-do-list"
                 projectTitle="To-Do App"
                 projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-                technologies={['JavaScript', 'HTML', 'CSS']}
+                technologies={['JavaScript', 'HTML', 'CSS', 'localStorage']}
             />
             <ProjectCard
                 src={library}
                 link="https://github.com/SrulyP/library-project"
                 projectTitle="Library App"
                 projectDesc="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-                technologies={['JavaScript', 'HTML', 'CSS']}
+                technologies={['JavaScript', 'HTML', 'CSS', 'localStorage']}
             />
         </section>
     );
