c35192c157dd05c7f1290e458044773cc4dc68b1
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-10-19 11:36:00 +0300

    Add footer information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,32 @@
 import '../App.css';
+import resume from '../assets/resume.pdf';
 
 function Footer() {
     return (
         <section className="footerSection">
+            <h3 className="inTouchTitle">Get In Touch</h3>
             <div className="inTouch">
-                <a>Email</a>
-                <a>LinkedIn</a>
-                <a>Github</a>
-                <a>Resume</a>
+                <a href="mailto:israelpolasak@gmail.com">Email</a>
+                <a
+                    href="https://linkedin.com/in/israelpolasak"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    LinkedIn
+                </a>
+                <a
+                    href="https://github.com/SrulyP"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub
+                </a>
+                <a href={resume} target="_blank">
+                    Resume
+                </a>
             </div>
             <div className="copyright">
-                &copy; Israel Polasak {new Date().getFullYear()} All rights
+                &copy; {new Date().getFullYear()} Israel Polasak | All rights
                 reserved.
             </div>
         </section>
